ca78f3ccf698e7a258a6254ec8a3fb59cc0d6c47
Hagai <hagai@newsound.mobi>
2018-10-19 16:45:53 +0300

    star

--- Settings.php
+++ Settings.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL); // or E_STRICT
 ini_set("display_errors",1);
 
 // Base Path
-Define('APP_PATH','/mvc');
+Define('APP_PATH','/tasks');
 
 // DB Settings
 Define('DB_NAME','dolce');
